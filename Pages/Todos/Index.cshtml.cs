388c4e1a9c77118ef77eb5dbb80003e34ba443a1
Kyle Rego <regoky@outlook.com>
2025-06-25 13:07:03 -0700

    Commit uncommited work

--- Pages/Todos/Index.cshtml.cs
+++ Pages/Todos/Index.cshtml.cs
@@ -4,12 +4,20 @@ public class IndexModel(AppDbContext db, UserManager<AppUser> users)
                                         : AppPageModel(db, users)
 {
     public List<Todo> Todos { get; set; } = [];
+    public List<Todo> Completed { get; set; } = [];
+    public List<Todo> Incompleted { get; set; } = [];
 
     public async Task<IActionResult> OnGetAsync()
     {
         string userId = UserId();
 
-        Todos = await _db.Todos.Where(t => t.UserId == userId).ToListAsync();
+        Todos = await _db.Todos.Include(t => t.Project)
+                        .Where(t => t.UserId == userId)
+                        .OrderBy(t => t.OnDate == null)
+                        .ThenBy(t => t.OnDate).ToListAsync();
+
+        Completed = [.. Todos.Where(t => t.Completed == true)];
+        Incompleted = [.. Todos.Where(t => t.Completed != true)];
 
         return Page();
     }                                    
