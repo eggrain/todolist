5cee5f79193acdee90d44d6f60783867d9a98e81
Kyle Rego <regoky@outlook.com>
2025-07-30 11:00:56 -0700

    Add delete progress notes

--- Pages/Goals/ProgressNotes/Edit.cshtml.cs
+++ Pages/Goals/ProgressNotes/Edit.cshtml.cs
@@ -37,10 +37,24 @@ public class EditModel(AppDbContext db, UserManager<AppUser> users)
         if (Note.UserId != userId) return BadRequest();
 
         ProgressNote? note = await _db.ProgressNotes.FirstOrDefaultAsync(
-            pn => pn.Id == noteId);
+            pn => pn.Id == noteId && pn.UserId == userId);
         if (note == null) return BadRequest();
         note.Text = Note.Text;
 
+        await _db.SaveChangesAsync();
+
+        return LocalRedirect($"/Goals/Show/{Note.GoalId}");
+    }
+
+    public async Task<IActionResult> OnPostDeleteAsync(string goalId, string noteId)
+    {
+        string userId = UserId();
+
+        ProgressNote? note = await _db.ProgressNotes.FirstOrDefaultAsync(
+            pn => pn.Id == noteId && pn.UserId == userId);
+        if (note == null) return BadRequest();
+
+        _db.ProgressNotes.Remove(note);
         await _db.SaveChangesAsync();
 
         return LocalRedirect($"/Goals/Show/{Note.GoalId}");
