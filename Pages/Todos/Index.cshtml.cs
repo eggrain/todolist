d9770a80eb4965e58d659e2345cc3e271fee9d2a
Kyle Rego <regoky@outlook.com>
2025-08-29 14:10:40 -0700

    Add paging, searching to todos index

--- Pages/Todos/Index.cshtml.cs
+++ Pages/Todos/Index.cshtml.cs
@@ -4,20 +4,46 @@ public class IndexModel(AppDbContext db, UserManager<AppUser> users)
                                         : AppPageModel(db, users)
 {
     public List<Todo> Todos { get; set; } = [];
-    public List<Todo> Completed { get; set; } = [];
-    public List<Todo> Incompleted { get; set; } = [];
 
-    public async Task<IActionResult> OnGetAsync()
+    public int PageNum { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public int TotalPages { get; set; }
+    public string Search { get; set; } = "";
+
+    [BindProperty(SupportsGet = true)] public bool ShowCompleted { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(int pageNum = 1, int pageSize = 30, string search = "")
     {
+        PageNum = Math.Max(1, pageNum);
+        PageSize = Math.Clamp(pageSize, 1, 50);
+        Search = search;
+
         string userId = UserId();
 
-        Todos = await _db.Todos.Include(t => t.Project).Include(t => t.Goal)
-                        .Where(t => t.UserId == userId)
-                        .OrderBy(t => t.OnDate == null)
-                        .ThenBy(t => t.OnDate).ToListAsync();
+        var baseQuery = _db.Todos
+                    .Where(t => t.UserId == userId)
+                    .AsNoTracking();
 
-        Completed = [.. Todos.Where(t => t.Completed == true)];
-        Incompleted = [.. Todos.Where(t => t.Completed != true)];
+        if (ShowCompleted == false)
+            baseQuery = baseQuery.Where(t => t.Completed == false);
+
+        if (!string.IsNullOrWhiteSpace(Search))
+            baseQuery = baseQuery.Where(t => t.Description.ToLower().Contains(Search.ToLower()));
+
+        TotalCount = await baseQuery.CountAsync(HttpContext.RequestAborted);
+        TotalPages = Math.Max((int)Math.Ceiling(TotalCount / (double)PageSize), 1);
+
+        if (PageNum > TotalPages) PageNum = TotalPages;
+
+        Todos = await baseQuery
+            .Include(t => t.Project)
+            .Include(t => t.Goal)
+            .OrderBy(t => t.OnDate == null)
+            .ThenBy(t => t.OnDate)
+            .Skip((PageNum - 1) * PageSize)
+            .Take(PageSize)
+            .ToListAsync(HttpContext.RequestAborted);
 
         return Page();
     }                                    
