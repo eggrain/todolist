9564066fdcca977c9c7e1144845c0e5fd177ef95
Kyle Rego <regoky@outlook.com>
2026-06-26 14:09:00 -0700

    Fix currency symbol issue

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using System.Globalization;
+
+var culture = new CultureInfo("en-US");
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
